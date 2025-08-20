08ef4ad687da79d33028e22e3a6bcc4b90d9662f
Tung Le <tungle@MacBook-Pro-cua-Tung.local>
2025-08-20 13:03:13 +0700

    set up project

---
+++ src/main/java/com/ecommerce/electronicapplicationmanagement/ElectronicApplicationManagementApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.electronicapplicationmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectronicApplicationManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElectronicApplicationManagementApplication.class, args);
+    }
+
+}
