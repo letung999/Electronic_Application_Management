517a26c2c45c375c60f9e170bf451b4788885ae1
Tung Le <tungle@MacBook-Pro-cua-Tung.local>
2025-08-23 08:38:07 +0700

    [Electronic Application] add converter for product

--- src/main/java/com/ecommerce/electronicapplicationmanagement/converter/ProductConverter.java
+++ src/main/java/com/ecommerce/electronicapplicationmanagement/converter/ProductConverter.java
@@ -1,7 +1,9 @@
 package com.ecommerce.electronicapplicationmanagement.converter;
 
+import com.ecommerce.electronicapplicationmanagement.dto.ProductDto;
 import com.ecommerce.electronicapplicationmanagement.entity.Product;
 import com.ecommerce.electronicapplicationmanagement.request.AddProductRequest;
+import com.ecommerce.electronicapplicationmanagement.request.BaseRequest;
 import org.mapstruct.Mapper;
 import org.mapstruct.ReportingPolicy;
 import org.mapstruct.factory.Mappers;
@@ -11,8 +13,14 @@ import java.util.List;
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface ProductConverter {
     ProductConverter INSTANCE = Mappers.getMapper(ProductConverter.class);
+
     Product fromProductRequest(AddProductRequest addProductRequest);
+
     AddProductRequest fromProduct(Product product);
-    List<Product> fromListProductRequest(List<AddProductRequest> addProductRequests);
-    List<AddProductRequest> fromListProductEntity(List<Product> productList);
+
+    List<ProductDto> fromListProduct(List<Product> products);
+
+    List<Product> fromListProductDto(List<ProductDto> productDtoList);
+
+    Product fromBaseRequest(BaseRequest baseRequest);
 }
