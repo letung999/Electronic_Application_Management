56142782825cf26abaaeefbb036021af3990ca25
Tung Le <tungle@MacBook-Pro-cua-Tung.local>
2025-08-24 12:17:46 +0700

    [Electronic Application] add message constaint

--- src/main/java/com/ecommerce/electronicapplicationmanagement/constant/MessageConstant.java
+++ src/main/java/com/ecommerce/electronicapplicationmanagement/constant/MessageConstant.java
@@ -7,4 +7,5 @@ public class MessageConstant {
     public static final String ERR_MSG_DATA_NOT_FOUND = "data is not found";
     public static final String ERR_MSG_CONFLICT_DATA_EXCEPTION = "occur conflict data exception";
     public static final String ERR_MSG_DATE_TIME_IS_INVALID = "date time is invalid!";
+    public static final String ERR_MSG_INSUFFICIENT_FOR_PRODUCT = "Insufficient stock for product";
 }
